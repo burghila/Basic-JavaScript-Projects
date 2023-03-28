0dfb4ba441d8260a1eca58d673fc5b80b9e2ed52
Alin Burghila <108270121+burghila@users.noreply.github.com>
2023-03-28 13:34:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
-// Declare the 'mesaj' variable and assign the string "Hello" to it
-var mesaj = "Hello";
-// Display a browser alert with the content of the 'mesaj' variable
-window.alert(mesaj);
-// Change the innerHTML of the HTML element with the id "mesaj" to the content of the 'mesaj' variable
-document.getElementById("mesaj").innerHTML = mesaj;
+ // Welcome alert
+        window.alert("Welcome to our website!");
 
-// Declare the 'mesaj2' variable and assign a string containing a quote and escaped single quotes
-var mesaj2 = 'John said, "I can\'t believe it\'s already Monday!"';
-// Declare three color variables and assign them color strings
-var color1 = "red";
-var color2 = "green";
-var color3 = "blue";
-// Write a paragraph to the document with text in the specified color using the fontcolor() method
-document.write("<p>This text is ".fontcolor(color1) + "red!</p>");
-document.write("<p>This text is ".fontcolor(color2) + "green!</p>");
-document.write("<p>This text is ".fontcolor(color3) + "blue!</p>");
+        // Variables to store two strings
+        var greeting = "Hello, ";
+        var name = "user!";
 
-// Declare variables for price, quantity, and total, and calculate the total as price * quantity
-var price = 10;
-var quantity = 5;
-var total = price * quantity;
+        // Concatenate the two string variables
+        var message = greeting + name;
 
-// Log the total value to the console
-console.log(total);
+        // Assign a color to the 'name' variable using the fontcolor() method
+        var coloredName = name.fontcolor("blue");
 
-// Change the innerHTML of the HTML element with the id "total" to the calculated total value
-document.getElementById("total").innerHTML = total;
+        // Write an expression (simple addition)
+        var expressionResult = 5 + 7;
+
+        // Display concatenated message and expression result using document.write()
+        document.write(message + " Here's an expression result: " + expressionResult + "<br>");
+        document.write("The colored name is: " + coloredName);
