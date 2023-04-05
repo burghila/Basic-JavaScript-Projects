2bb672130d6b3b2cc2b9de969edfa2cac4d7830f
Alin Burghila <108270121+burghila@users.noreply.github.com>
2023-04-05 20:48:57 +0100

    Update main.js

--- Project7_scope_time_function/main.js
+++ Project7_scope_time_function/main.js
@@ -1,46 +1,54 @@
 // Declare global variable
-let globalVar = "I'm a global variable";
+    let globalVar = "I'm a global variable";
 
-// Declare local variable inside a function
-function localVarFunc() {
-  let localVar = "I'm a local variable";
-  console.log(localVar);
-}
+    // Declare local variable inside a function
+    function localVarFunc() {
+      let localVar = "I'm a local variable";
+      displayOutput(localVar);
+    }
 
-// Function that includes an if statement
-function checkNum(num) {
-  if (num > 10) {
-    console.log("The number is greater than 10.");
-  } else {
-    console.log("The number is 10 or less.");
-  }
-}
+    // Function that includes an if statement
+    function checkNum(num) {
+      if (num > 10) {
+        displayOutput("The number is greater than 10.");
+      } else {
+        displayOutput("The number is 10 or less.");
+      }
+    }
 
-// Intentionally write a function with an error and use console.log() for debugging
-function buggyFunc() {
-  try {
-    let a = 10;
-    let b = 0;
-    let result = a / b;
-    console.log(result);
-  } catch (error) {
-    console.log("An error occurred:", error);
-  }
-}
+    // Intentionally write a function with an error and use console.log() for debugging
+    function buggyFunc() {
+      try {
+        let a = 10;
+        let b = 0;
+        let result = a / b;
+        displayOutput(result);
+      } catch (error) {
+        displayOutput("An error occurred: " + error);
+      }
+    }
 
-// Time_function as outlined in previous slides
-function timeFunction() {
-  let date = new Date();
-  let hour = date.getHours();
-  let minute = date.getMinutes();
-  let second = date.getSeconds();
-  
-  let timeString = `Current time: ${hour}:${minute}:${second}`;
-  console.log(timeString);
-}
+    // Time_function as outlined in previous slides
+    function timeFunction() {
+      let date = new Date();
+      let hour = date.getHours();
+      let minute = date.getMinutes();
+      let second = date.getSeconds();
+      
+      let timeString = `Current time: ${hour}:${minute}:${second}`;
+      displayOutput(timeString);
+    }
 
-// Call the functions
-localVarFunc();
-checkNum(15);
-buggyFunc();
-timeFunction();
+    // Function to display output on the web page
+    function displayOutput(message) {
+      let outputElement = document.getElementById("output");
+      outputElement.innerHTML += message + "<br>";
+    }
+	
+	    // Function to run all other functions when the button is clicked
+    function runFunctions() {
+      localVarFunc();
+      checkNum(15);
+      buggyFunc();
+      timeFunction();
+    }
